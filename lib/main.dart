767a35a80d8c1b17af819bebe979c6ffec414f8d
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-02-28 17:12:07 +0100

    Layouted basic patient screen (main screen).

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,175 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return PEBRApp();
+  }
+}
+
+class PEBRApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'PEBRApp',
+      theme: ThemeData.light(),
+      home: _home(context),
     );
   }
+
+  _home(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: _customHeightAppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.add),
+      ),
+      body: _buildPatientTable(context),
+    );
+  }
+
+  _customHeightAppBar() {
+    return AppBar(
+      backgroundColor: Color.fromARGB(0, 0, 0, 0),
+      elevation: 0,
+      // This is a hack so we can increase the height of the AppBar
+      bottom: PreferredSize(
+        child: Container(),
+        preferredSize: Size.fromHeight(35),
+      ),
+      flexibleSpace: PageHeader(title: 'Patients', subtitle: 'Overview'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+    _buildPatientTable(BuildContext context) {
+      return ListView(
+        children: _buildPatientCards(context),
+      );
+    }
+
+    _buildPatientCards(BuildContext context) {
+      final _paddingVertical = 10.0;
+      final _paddingHorizontal = 10.0;
+
+      var _patients = <Widget>[];
+      final numberOfPatients = 20;
+      for (var i = 0; i < numberOfPatients; i++) {
+        _patients.add(Card(
+          margin: i == numberOfPatients - 1
+              ? EdgeInsets.only(
+              top: _paddingVertical,
+              bottom: _paddingVertical,
+              left: _paddingHorizontal,
+              right: _paddingHorizontal)
+              : EdgeInsets.only(
+              top: _paddingVertical,
+              bottom: 0,
+              left: _paddingHorizontal,
+              right: _paddingHorizontal),
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+              onTap: () {
+                print('Card was tapped');
+              },
+              // Generally, material cards use onSurface with 12% opacity for the pressed state.
+              splashColor:
+              Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
+              // Generally, material cards do not have a highlight overlay.
+              highlightColor: Colors.transparent,
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  child: Row(
+                    children: <Widget>[
+                      _formatText('B/01/00378'),
+                      _formatText('02.02.2019'),
+                      _formatText('VHW'),
+                      Icon(Icons.home),
+                      _formatText('Unsuppr'),
+                      _formatText('Today'),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ))),
+        ));
+      }
+      return _patients;
+    }
+
+    _formatText(String text) {
+      return Text(
+        text,
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      );
+    }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class PageHeader extends StatelessWidget {
+  String _title, _subtitle;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  PageHeader({String title, String subtitle}) {
+    this._title = title;
+    this._subtitle = subtitle;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+          child: _buildTitleAndSubtitle(_title, _subtitle)
         ),
+    );
+  }
+
+  _buildTitleAndSubtitle(String title, String subtitle) {
+    if (title == null && subtitle == null) {
+      return null;
+    }
+
+    if (title != null) {
+      if (subtitle != null) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _formatTitle(title),
+            _formatSubtitle(subtitle),
+          ],
+        );
+      } else {
+        return _formatTitle(title);
+      }
+    }
+  }
+
+  _formatTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: 36,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  _formatSubtitle(String subtitle) {
+    return Text(
+      subtitle,
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
     );
   }
 }
