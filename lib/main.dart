fbf01f10ed765201f9faa6fc8c09651bdbe13a2a
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-02-28 21:55:49 +0100

    Made AppBar transparent and blurry, list scrolls behind AppBar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
 
 class PEBRApp extends StatelessWidget {
 
+  final _appBarHeight = 115.0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,25 +25,36 @@ class PEBRApp extends StatelessWidget {
 
   _home(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: _customHeightAppBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        child: Icon(Icons.add),
-      ),
-      body: _buildPatientTable(context),
-    );
+        backgroundColor: Color.fromARGB(255, 224, 224, 224),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Icon(Icons.add),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _buildPatientTable(context),
+            Container(
+              height: _appBarHeight,
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: new ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: _customHeightAppBar(),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 
   _customHeightAppBar() {
     return AppBar(
-      backgroundColor: Color.fromARGB(0, 0, 0, 0),
+      backgroundColor: Colors.transparent,
       elevation: 0,
       // This is a hack so we can increase the height of the AppBar
-      bottom: PreferredSize(
-        child: Container(),
-        preferredSize: Size.fromHeight(35),
-      ),
+//      bottom: PreferredSize(
+//        child: Container(),
+//        preferredSize: Size.fromHeight(35),
+//      ),
       flexibleSpace: PageHeader(title: 'Patients', subtitle: 'Overview'),
       actions: <Widget>[
         IconButton(
@@ -51,68 +65,73 @@ class PEBRApp extends StatelessWidget {
     );
   }
 
-    _buildPatientTable(BuildContext context) {
-      return ListView(
-        children: _buildPatientCards(context),
-      );
+  _buildPatientTable(BuildContext context) {
+    return ListView(
+      children: _buildPatientCards(context),
+    );
+  }
+
+  _buildPatientCards(BuildContext context) {
+    final _paddingVertical = 10.0;
+    final _paddingHorizontal = 10.0;
+
+    var _patients = <Widget>[
+      // container acting as margin for the app bar
+      Container(
+        height: _appBarHeight - 10,
+        color: Colors.transparent,
+      )
+    ];
+    final numberOfPatients = 20;
+    for (var i = 0; i < numberOfPatients; i++) {
+      _patients.add(Card(
+        margin: i == numberOfPatients - 1
+            ? EdgeInsets.only(
+                top: _paddingVertical,
+                bottom: _paddingVertical,
+                left: _paddingHorizontal,
+                right: _paddingHorizontal)
+            : EdgeInsets.only(
+                top: _paddingVertical,
+                bottom: 0,
+                left: _paddingHorizontal,
+                right: _paddingHorizontal),
+        clipBehavior: Clip.antiAlias,
+        child: InkWell(
+            onTap: () {
+              print('Card was tapped');
+            },
+            // Generally, material cards use onSurface with 12% opacity for the pressed state.
+            splashColor:
+                Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
+            // Generally, material cards do not have a highlight overlay.
+            highlightColor: Colors.transparent,
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Row(
+                  children: <Widget>[
+                    _formatText('B/01/00378'),
+                    _formatText('02.02.2019'),
+                    _formatText('VHW'),
+                    Icon(Icons.home),
+                    _formatText('Unsuppr'),
+                    _formatText('Today'),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                ))),
+      ));
     }
+    return _patients;
+  }
 
-    _buildPatientCards(BuildContext context) {
-      final _paddingVertical = 10.0;
-      final _paddingHorizontal = 10.0;
-
-      var _patients = <Widget>[];
-      final numberOfPatients = 20;
-      for (var i = 0; i < numberOfPatients; i++) {
-        _patients.add(Card(
-          margin: i == numberOfPatients - 1
-              ? EdgeInsets.only(
-              top: _paddingVertical,
-              bottom: _paddingVertical,
-              left: _paddingHorizontal,
-              right: _paddingHorizontal)
-              : EdgeInsets.only(
-              top: _paddingVertical,
-              bottom: 0,
-              left: _paddingHorizontal,
-              right: _paddingHorizontal),
-          clipBehavior: Clip.antiAlias,
-          child: InkWell(
-              onTap: () {
-                print('Card was tapped');
-              },
-              // Generally, material cards use onSurface with 12% opacity for the pressed state.
-              splashColor:
-              Theme.of(context).colorScheme.onSurface.withOpacity(0.12),
-              // Generally, material cards do not have a highlight overlay.
-              highlightColor: Colors.transparent,
-              child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                  child: Row(
-                    children: <Widget>[
-                      _formatText('B/01/00378'),
-                      _formatText('02.02.2019'),
-                      _formatText('VHW'),
-                      Icon(Icons.home),
-                      _formatText('Unsuppr'),
-                      _formatText('Today'),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  ))),
-        ));
-      }
-      return _patients;
-    }
-
-    _formatText(String text) {
-      return Text(
-        text,
-        style: TextStyle(
-          fontSize: 18,
-        ),
-      );
-    }
-
+  _formatText(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 18,
+      ),
+    );
+  }
 }
 
 class PageHeader extends StatelessWidget {
@@ -126,10 +145,9 @@ class PageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
+      child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-          child: _buildTitleAndSubtitle(_title, _subtitle)
-        ),
+          child: _buildTitleAndSubtitle(_title, _subtitle)),
     );
   }
 
