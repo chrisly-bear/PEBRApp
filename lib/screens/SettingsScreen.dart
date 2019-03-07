91efcda9fd309ceaf75b5cd0c46b4369a9095130
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-03-07 08:57:04 +0100

    Added "Backup" and "Set PIN" buttons to settings screen.

--- lib/screens/SettingsScreen.dart
+++ lib/screens/SettingsScreen.dart
@@ -7,8 +7,31 @@ class SettingsScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Settings'),
         ),
-        body: Center(
-          child: Text('SETTINGS SCREEN'),
-        ));
+        body: Center(child: SettingsBody()));
+  }
+}
+
+class SettingsBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          width: 150,
+          child: RaisedButton(
+            onPressed: () {},
+            child: Text('Set PIN'),
+          ),
+        ),
+        SizedBox(
+          width: 150,
+          child: RaisedButton(
+            onPressed: () {},
+            child: Text('Start Backup'),
+          ),
+        ),
+      ],
+    );
   }
 }
