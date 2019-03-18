9b70cb2bed0781d820f04fa851a13aa2ed119803
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-03-18 16:12:29 +0100

    Display real data from database on patient screen.

--- lib/screens/PatientScreen.dart
+++ lib/screens/PatientScreen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pebrapp/components/SizedButton.dart';
 import 'package:pebrapp/database/models/Patient.dart';
+import 'package:pebrapp/database/models/PreferenceAssessment.dart';
 import 'package:pebrapp/screens/NewOrEditPatientScreen.dart';
 import 'package:pebrapp/screens/PreferenceAssessmentScreen.dart';
+import 'package:pebrapp/utils/Utils.dart';
 
 class PatientScreen extends StatelessWidget {
   final Patient _patient;
@@ -145,6 +147,121 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
   }
 
   _buildPreferencesCard() {
+
+    _buildARTRefillText() {
+      String text = artRefillOptionToString(_patient.latestPreferenceAssessment.artRefillOption1);
+      return Text(text);
+    }
+
+    _buildAdherenceReminderMessageText() {
+      String text = _patient.latestPreferenceAssessment.adherenceReminderMessage ?? '—';
+      return Text(text);
+    }
+
+    _buildAdherenceReminderFrequencyText() {
+      final AdherenceReminderFrequency freq = _patient.latestPreferenceAssessment.adherenceReminderFrequency;
+      String text = freq == null ? '—' : adherenceReminderFrequencyToString(freq);
+      return Text(text);
+    }
+
+    _buildAdherenceReminderTimeText() {
+      String text = _patient.latestPreferenceAssessment.adherenceReminderTime ?? '—';
+      return Text(text);
+    }
+
+    _buildVLMessageSuppressedText() {
+      String text = _patient.latestPreferenceAssessment.vlNotificationMessageSuppressed ?? '—';
+      return Text(text);
+    }
+
+    _buildVLMessageUnsuppressedText() {
+      String text = _patient.latestPreferenceAssessment.vlNotificationMessageUnsuppressed ?? '—';
+      return Text(text);
+    }
+
+    TableRow _buildSupportOption(String title) {
+      return TableRow(children: [
+        TableCell(
+          child: CheckboxListTile(
+            // secondary: const Icon(Icons.home),
+            title: Text(title),
+            dense: true,
+            value: false,
+            onChanged: (bool newState) {
+              print('Checkbox clicked: $newState');
+            },
+          ),
+        ),
+        TableCell(
+          verticalAlignment: TableCellVerticalAlignment.middle,
+          child: Text(''),
+        ),
+      ]);
+    }
+
+    _buildSupportOptions() {
+      final SupportPreferencesSelection sps = _patient.latestPreferenceAssessment.supportPreferences;
+      if (sps.areAllDeselected) {
+        return Text('—');
+      }
+      List<TableRow> supportOptions = List<TableRow>();
+      if (sps.saturdayClinicClubSelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.saturdayClinicClubDescription));
+      }
+      if (sps.communityYouthClubSelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.communityYouthClubDescription));
+      }
+      if (sps.phoneCallPESelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.phoneCallPEDescription));
+      }
+      if (sps.homeVisitPESelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.homeVisitPEDescription));
+      }
+      if (sps.schoolTalkPESelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.schoolTalkPEDescription));
+      }
+      if (sps.nurseAtClinicSelected) {
+        supportOptions.add(_buildSupportOption(SupportPreferencesSelection.nurseAtClinicDescription));
+      }
+
+      // TODO: remove this demo option (it is just an idea how to display completed items)
+      supportOptions.add(
+        TableRow(children: [
+          TableCell(
+            verticalAlignment: TableCellVerticalAlignment.middle,
+            child: CheckboxListTile(
+              // secondary: const Icon(Icons.local_hospital),
+              title: Text(
+                'Completed Item',
+                style: TextStyle(
+                    decoration: TextDecoration.lineThrough),
+              ),
+              dense: true,
+              value: true,
+              onChanged: (bool newState) {
+                print('Checkbox clicked: $newState');
+              },
+            ),
+          ),
+          TableCell(
+            verticalAlignment: TableCellVerticalAlignment.middle,
+            child: Text('done on 04.02.2019'),
+          ),
+        ]),
+      );
+
+      return Table(columnWidths: {
+        0: FixedColumnWidth(250.0),
+        // 0: IntrinsicColumnWidth(),
+        // 1: FixedColumnWidth(250.0),
+      }, children: supportOptions);
+    }
+
+    _buildEACText() {
+      String text = eacOptionToString(_patient.latestPreferenceAssessment.eacOption) ?? '—';
+      return Text(text);
+    }
+
     if (_patient.latestPreferenceAssessment == null) {
       return Card(
           margin: EdgeInsets.symmetric(horizontal: 15),
@@ -176,7 +293,7 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('VHW')),
+                      child: _buildARTRefillText()),
                 ),
               ]),
               TableRow(children: [
@@ -184,25 +301,37 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                     child: Padding(
                         padding: EdgeInsets.symmetric(
                             vertical: _tableRowPaddingVertical),
-                        child: Text('Weekly Notification Message'))),
+                        child: Text('Adherence Reminder Message'))),
                 TableCell(
                     child: Padding(
                         padding: EdgeInsets.symmetric(
                             vertical: _tableRowPaddingVertical),
-                        child: Text('—'))),
+                        child: _buildAdherenceReminderMessageText())),
+              ]),
+              TableRow(children: [
+                TableCell(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: _tableRowPaddingVertical),
+                        child: Text('Adherence Reminder Frequency'))),
+                TableCell(
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            vertical: _tableRowPaddingVertical),
+                        child: _buildAdherenceReminderFrequencyText())),
               ]),
               TableRow(children: [
                 TableCell(
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('Weekly Notification Time')),
+                      child: Text('Adherence Reminder Notification Time')),
                 ),
                 TableCell(
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('—')),
+                      child: _buildAdherenceReminderTimeText()),
                 ),
               ]),
               TableRow(children: [
@@ -216,7 +345,7 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('You rock!')),
+                      child: _buildVLMessageSuppressedText()),
                 ),
               ]),
               TableRow(children: [
@@ -230,7 +359,7 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text(':(')),
+                      child: _buildVLMessageUnsuppressedText()),
                 ),
               ]),
               TableRow(children: [
@@ -244,54 +373,7 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                   child: Padding(
                     padding: EdgeInsets.symmetric(
                         vertical: _tableRowPaddingVertical),
-                    child: Table(columnWidths: {
-                      0: FixedColumnWidth(250.0),
-                      // 0: IntrinsicColumnWidth(),
-                      // 1: FixedColumnWidth(250.0),
-                    }, children: [
-
-                      TableRow(children: [
-                        TableCell(
-                          child: CheckboxListTile(
-                            // secondary: const Icon(Icons.home),
-                            title: Text('Home Visit PE'),
-                            dense: true,
-                            value: false,
-                            onChanged: (bool newState) {
-                              print('Checkbox clicked: $newState');
-                            },
-                          ),
-                        ),
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text(''),
-                        ),
-                      ]),
-
-                      TableRow(children: [
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: CheckboxListTile(
-                            // secondary: const Icon(Icons.local_hospital),
-                            title: Text(
-                              'Nurse at Clinic',
-                              style: TextStyle(
-                                  decoration: TextDecoration.lineThrough),
-                            ),
-                            dense: true,
-                            value: true,
-                            onChanged: (bool newState) {
-                              print('Checkbox clicked: $newState');
-                            },
-                          ),
-                        ),
-                        TableCell(
-                          verticalAlignment: TableCellVerticalAlignment.middle,
-                          child: Text('done on 04.02.2019'),
-                        ),
-                      ]),
-
-                    ]),
+                    child: _buildSupportOptions(),
                   ),
                 ),
               ]),
@@ -300,18 +382,19 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('EAC')),
+                      child: Text('EAC (Enhanced Adherence Counseling)')),
                 ),
                 TableCell(
                   child: Padding(
                       padding: EdgeInsets.symmetric(
                           vertical: _tableRowPaddingVertical),
-                      child: Text('Nurse at Clinic')),
+                      child: _buildEACText()),
                 ),
               ]),
             ],
           )),
     );
+
   }
 
   void _pushPreferenceAssessmentScreen(BuildContext context, String patientART) {
