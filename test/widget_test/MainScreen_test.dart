c62bdcbad235bc53baa05a3f1a7e75c9964442b5
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-03-22 10:37:12 +0100

    Divided test directory into unit_test and widget_test subdirectories. Added basic widget test which looks for the 'add patient' button on the main screen. Run with `flutter test`.

---
+++ test/widget_test/MainScreen_test.dart
@@ -0,0 +1,26 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pebrapp/screens/MainScreen.dart';
+
+void main() {
+  testWidgets("Main Screen has an 'add patient' button", (WidgetTester tester) async {
+    // wrap MainScreen in a MediaQuery
+    Widget testWidget = new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(home: MainScreen())
+    );
+
+    await tester.pumpWidget(testWidget);
+
+    final Finder addButtonFinder = find.byIcon(Icons.add);
+    expect(addButtonFinder, findsOneWidget);
+  });
+}
