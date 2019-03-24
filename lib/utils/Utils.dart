b5f26ab7be5ced51d7effa88a793a5add870dccc
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-03-24 01:38:36 +0100

    We don't need to get cookies or anything for file upload, we can make a PUT directly. Created login method (can be used to check if credentials are valid).

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:pebrapp/database/models/PreferenceAssessment.dart';
 import 'package:intl/intl.dart';
-import 'package:html/parser.dart' show parse;
-import 'package:html/dom.dart' as dom;
 
 void showFlushBar(BuildContext context, String message, {String title}) {
   Flushbar(
@@ -129,8 +127,7 @@ DateTime calculateNextARTRefill(DateTime lastARTRefill) {
   return lastARTRefill.add(Duration(days: 90));
 }
 
-Future<void> uploadDatabaseToSWITCHdrive(File sourceFile, String targetFolder,
-    String targetFilename, String username, String password) async {
+Future<void> loginToSWITCHdrive(String username, String password) async {
 
   String _urlEncode(Map data) {
     return data.keys.map((key) => "${Uri.encodeComponent(key)}=${Uri.encodeComponent(data[key])}").join("&");
@@ -140,49 +137,46 @@ Future<void> uploadDatabaseToSWITCHdrive(File sourceFile, String targetFolder,
     return Uri.encodeComponent(s);
   }
 
+  final _switchHost = 'drive.switch.ch';
+  // https://drive.switch.ch/index.php/login
+  final _loginUri = Uri.https(_switchHost, 'index.php/login');
+
+  // log in
+  final _payload = _urlEncode({
+    'user': username,
+    'password': password,
+  });
+  final _contentLength = utf8.encode(_payload).length;
+  final request = await HttpClient().postUrl(_loginUri)
+    ..headers.add('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3')
+    ..contentLength = _contentLength
+    ..write(_payload);
+  final response = await request.close();
+
+  final statusCode = response.statusCode;
+  print("end of login (status code: $statusCode)");
+}
+
+Future<void> uploadDatabaseToSWITCHdrive(File sourceFile, String targetFolder,
+    String targetFilename, String username, String password) async {
+
   String _base64Encode(String s) {
     final bytes = utf8.encode(s);
     return base64.encode(bytes);
   }
 
   final _switchHost = 'drive.switch.ch';
-  // https://drive.switch.ch/index.php/login
-  final _loginUri = Uri.https(_switchHost, 'index.php/login');
-
   // https://drive.switch.ch/remote.php/webdav/targetFolder/targetFilename
   final _uploadPath = 'remote.php/webdav/' + targetFolder + '/' + targetFilename;
   final _uploadUri = Uri.https(_switchHost, _uploadPath);
 
-  // get request token
-  String requestToken;
-  HttpClientRequest request = await HttpClient().getUrl(_loginUri);
-  HttpClientResponse response = await request.close();
-  await response.transform(utf8.decoder).forEach((String html) {
-    final dom.Document doc = parse(html);
-    final dom.Element el = doc.querySelector('input[name="requesttoken"]');
-    requestToken = el.attributes['value'];
-  });
-
-  // log in
-  final _payload = _urlEncode({
-    'user': username,
-    'password': password,
-    'requesttoken': requestToken,
-  });
-  final _contentLength = utf8.encode(_payload).length;
-  request = await HttpClient().postUrl(_loginUri)
-    ..headers.add('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3')
-    ..contentLength = _contentLength
-    ..write(_payload);
-  response = await request.close();
-
   // upload file
-  request = await HttpClient().putUrl(_uploadUri)
+  final request = await HttpClient().putUrl(_uploadUri)
     ..headers.add('Authorization', 'Basic ${_base64Encode(username + ':' + password)}')
     ..contentLength = await sourceFile.length()
     ..add(sourceFile.readAsBytesSync())
   ;
-  response = await request.close();
+  final response = await request.close();
 
   final statusCode = response.statusCode;
   print("end of upload (status code: $statusCode)");
