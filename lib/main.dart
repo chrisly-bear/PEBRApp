e7319cd3fd1d73ecb98deac46ccbcba091f975ae
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-05-02 10:30:21 +0200

    Listen to app lifecycle changes to trigger automatic backup in the background. Still needs implementation of the actual backup.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,34 @@ import 'package:pebrapp/screens/MainScreen.dart';
 
 void main() => runApp(PEBRApp());
 
-class PEBRApp extends StatelessWidget {
+class PEBRApp extends StatefulWidget {
+  @override
+  _PEBRAppState createState() => _PEBRAppState();
+}
+
+class _PEBRAppState extends State<PEBRApp> with WidgetsBindingObserver {
+
+  @override
+  void initState() {
+    super.initState();
+    // listen to changes in the app lifecycle
+    WidgetsBinding.instance.addObserver(this);
+    _runBackupAndUploadCSVToSWITCH();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _runBackupAndUploadCSVToSWITCH();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,5 +38,12 @@ class PEBRApp extends StatelessWidget {
         theme: ThemeData.light(),
         home: MainScreen()
     );
+  }
+
+  Future<void> _runBackupAndUploadCSVToSWITCH() async {
+    print("### Attempting backup... ###");
+    // TODO: run backup if the last successful backup dates back more than a day
+    await Future.delayed(Duration(seconds: 5));
+    print("### Backup complete! ###");
   }
 }
