1a4eea970979278b8120112e3abecd1dc64f60a9
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-06-13 10:57:01 +0200

    Reordered code, helper functions are now at the bottom of the class. Added tick off functionality (which is purely visual right now, nothing is saved in the background).

--- lib/screens/PatientScreen.dart
+++ lib/screens/PatientScreen.dart
@@ -54,6 +54,16 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
   String _nextAssessmentText = '—';
   String _nextRefillText = '—';
 
+  bool NURSE_CLINIC_done = false;
+  bool SATURDAY_CLINIC_CLUB_done = false;
+  bool COMMUNITY_YOUTH_CLUB_done = false;
+  bool PHONE_CALL_PE_done = false;
+  bool HOME_VISIT_PE_done = false;
+  bool SCHOOL_VISIT_PE_done = false;
+  bool PITSO_VISIT_PE_done = false;
+
+  bool dummy_item_done = false;
+
   _PatientScreenBodyState(this._context, this._patient);
   
   @override
@@ -199,43 +209,6 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
       );
     }
 
-    Widget _makeSubtitle(String subtitle) {
-      return Padding(padding: EdgeInsets.only(top: 20, bottom: 10),
-          child:
-          Row(children: [
-            Text(subtitle, style: TextStyle(
-              color: DATA_SUBTITLE_TEXT,
-              fontStyle: FontStyle.italic,
-              fontSize: 15.0,
-            ),)
-          ]));
-    }
-
-    ClipRect _getPaddedIcon(String assetLocation, {Color color}) {
-      return ClipRect(
-        clipBehavior: Clip.antiAlias,
-        child: SizedOverflowBox(
-          size: Size(32.0, 30.0),
-          child: Image(
-            height: 30.0,
-            color: color,
-            image: AssetImage(assetLocation),
-          ),
-        ),
-      );
-    }
-
-    Widget _formatHeaderRowText(String text) {
-      return Text(
-        text.toUpperCase(),
-        style: TextStyle(
-          fontSize: 12.0,
-          color: VL_HISTORY_HEADER_TEXT,
-          fontWeight: FontWeight.w600,
-        ),
-      );
-    }
-
     Widget _buildViralLoadHeader() {
       Widget content = Row(
         children: <Widget>[
@@ -265,9 +238,10 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
     Widget _buildViralLoadRow(vl) {
       if (vl?.isSuppressed == null) { return Column(); }
       Widget description = Text('${formatDateConsistent(vl.dateOfBloodDraw)}');
+      final double vlIconSize = 25.0;
       Widget viralLoadIcon = vl.isSuppressed
-          ? _getPaddedIcon('assets/icons/viralload_suppressed.png')
-          : _getPaddedIcon('assets/icons/viralload_unsuppressed.png');
+          ? _getPaddedIcon('assets/icons/viralload_suppressed.png', width: vlIconSize, height: vlIconSize)
+          : _getPaddedIcon('assets/icons/viralload_unsuppressed.png', width: vlIconSize, height: vlIconSize);
       Widget viralLoadBadge = ViralLoadBadge(vl, smallSize: false);
       Widget content = Row(
         children: <Widget>[
@@ -354,130 +328,75 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
     );
   }
 
-  TableRow _buildTableRow(String description, String content) {
-    return _buildTableRowWithWidget(description, Text(content ?? '—'));
-  }
-
-  TableRow _buildTableRowWithWidget(String description, Widget content) {
-    return TableRow(children: [
-      TableCell(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-              vertical: _tableRowPaddingVertical),
-          child: Text(description),
-        ),
-      ),
-      TableCell(
-        child: Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: _tableRowPaddingVertical),
-            child: content),
-      ),
-    ]);
-  }
-
   _buildPreferencesCard() {
-
-    TableRow _buildSupportOption(String title, {Widget icon}) {
-      return TableRow(children: [
-        TableCell(
-          child: CheckboxListTile(
-            secondary: icon,
-            title: Text(title),
-            dense: true,
-            value: false,
-            onChanged: (bool newState) {
-              print('Checkbox clicked: $newState');
-            },
-          ),
-        ),
-        TableCell(
-          verticalAlignment: TableCellVerticalAlignment.middle,
-          child: Text(''),
-        ),
-      ]);
-    }
-
-    ClipRect _getPaddedIcon(String assetLocation, {Color color}) {
-      return ClipRect(
-          clipBehavior: Clip.antiAlias,
-          child: SizedOverflowBox(
-              size: Size(25.0, 25.0),
-              child: Image(
-                height: 25.0,
-                color: color,
-                image: AssetImage(
-                    assetLocation),
-              )));
-    }
 
     _buildSupportOptions() {
       final SupportPreferencesSelection sps = _patient.latestPreferenceAssessment.supportPreferences;
       if (sps.areAllDeselected) {
         return Text('—');
       }
+      final double iconWidth = 28.0;
+      final double iconHeight = 28.0;
       List<TableRow> supportOptions = List<TableRow>();
       if (sps.NURSE_CLINIC_selected) {
         supportOptions.add(
             _buildSupportOption(SupportPreferencesSelection.NURSE_CLINIC_DESCRIPTION,
-                icon: _getPaddedIcon('assets/icons/nurse_clinic_fett.png')
+                checkboxState: NURSE_CLINIC_done,
+                onChanged: (bool newState) { setState(() { NURSE_CLINIC_done = newState; }); },
+                icon: _getPaddedIcon('assets/icons/nurse_clinic_fett.png', width: iconWidth, height: iconHeight),
             ));
       }
       if (sps.SATURDAY_CLINIC_CLUB_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.SATURDAY_CLINIC_CLUB_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/saturday_clinic_club_black.png')
+            checkboxState: SATURDAY_CLINIC_CLUB_done,
+            onChanged: (bool newState) { setState(() { SATURDAY_CLINIC_CLUB_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/saturday_clinic_club_black.png', width: iconWidth, height: iconHeight)
         ));
       }
       if (sps.COMMUNITY_YOUTH_CLUB_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.COMMUNITY_YOUTH_CLUB_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/youth_club_black.png')
+            checkboxState: COMMUNITY_YOUTH_CLUB_done,
+            onChanged: (bool newState) { setState(() { COMMUNITY_YOUTH_CLUB_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/youth_club_black.png', width: iconWidth, height: iconHeight)
         ));
       }
       if (sps.PHONE_CALL_PE_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.PHONE_CALL_PE_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/phonecall_pe_black.png')
+            checkboxState: PHONE_CALL_PE_done,
+            onChanged: (bool newState) { setState(() { PHONE_CALL_PE_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/phonecall_pe_black.png', width: iconWidth, height: iconHeight)
         ));
       }
       if (sps.HOME_VISIT_PE_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.HOME_VISIT_PE_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/homevisit_pe_black.png')
+            checkboxState: HOME_VISIT_PE_done,
+            onChanged: (bool newState) { setState(() { HOME_VISIT_PE_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/homevisit_pe_black.png', width: iconWidth, height: iconHeight)
         ));
       }
       if (sps.SCHOOL_VISIT_PE_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.SCHOOL_VISIT_PE_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/schooltalk_pe_black.png')
+            checkboxState: SCHOOL_VISIT_PE_done,
+            onChanged: (bool newState) { setState(() { SCHOOL_VISIT_PE_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/schooltalk_pe_black.png', width: iconWidth, height: iconHeight)
         ));
       }
       if (sps.PITSO_VISIT_PE_selected) {
         supportOptions.add(_buildSupportOption(SupportPreferencesSelection.PITSO_VISIT_PE_DESCRIPTION,
-            icon: _getPaddedIcon('assets/icons/pitso_black.png')
+            checkboxState: PITSO_VISIT_PE_done,
+            onChanged: (bool newState) { setState(() { PITSO_VISIT_PE_done = newState; }); },
+            icon: _getPaddedIcon('assets/icons/pitso_black.png', width: iconWidth, height: iconHeight)
         ));
       }
 
       // TODO: remove this demo option (it is just an idea how to display completed items)
       supportOptions.add(
-        TableRow(children: [
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: CheckboxListTile(
-              // secondary: const Icon(Icons.local_hospital),
-              title: Text(
-                'Completed Item',
-                style: TextStyle(
-                    decoration: TextDecoration.lineThrough),
-              ),
-              dense: true,
-              value: true,
-              onChanged: (bool newState) {
-                print('Checkbox clicked: $newState');
-              },
-            ),
-          ),
-          TableCell(
-            verticalAlignment: TableCellVerticalAlignment.middle,
-            child: Text('done on 04.02.2019'),
-          ),
-        ]),
+          _buildSupportOption(
+              'Completed Item',
+              checkboxState: dummy_item_done,
+              onChanged: (bool newState) { setState(() { dummy_item_done = newState; }); },
+              icon: _getPaddedIcon('assets/icons/schooltalk_pe_black.png', width: iconWidth, height: iconHeight), doneText: 'done on 04.02.2019',
+          )
       );
 
       return Table(columnWidths: {
@@ -526,14 +445,99 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
 
   }
 
-  void _pushPreferenceAssessmentScreen(BuildContext context, Patient patient) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return PreferenceAssessmentScreen(patient);
-        },
+
+  /*
+   * Helper Functions
+   */
+
+  TableRow _buildTableRow(String description, String content) {
+    return _buildTableRowWithWidget(description, Text(content ?? '—'));
+  }
+
+  TableRow _buildTableRowWithWidget(String description, Widget content) {
+    return TableRow(children: [
+      TableCell(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              vertical: _tableRowPaddingVertical),
+          child: Text(description),
+        ),
+      ),
+      TableCell(
+        child: Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: _tableRowPaddingVertical),
+            child: content),
+      ),
+    ]);
+  }
+
+  Widget _formatHeaderRowText(String text) {
+    return Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        fontSize: 12.0,
+        color: VL_HISTORY_HEADER_TEXT,
+        fontWeight: FontWeight.w600,
       ),
     );
+  }
+
+  Widget _makeSubtitle(String subtitle) {
+    return Padding(padding: EdgeInsets.only(top: 20, bottom: 10),
+        child:
+        Row(children: [
+          Text(subtitle, style: TextStyle(
+            color: DATA_SUBTITLE_TEXT,
+            fontStyle: FontStyle.italic,
+            fontSize: 15.0,
+          ),)
+        ]));
+  }
+
+  ClipRect _getPaddedIcon(String assetLocation, {Color color, double width: 25.0, double height: 25.0}) {
+    return ClipRect(
+        clipBehavior: Clip.antiAlias,
+        child: SizedOverflowBox(
+            size: Size(width, height),
+            child: Image(
+              height: height,
+              color: color,
+              image: AssetImage(
+                  assetLocation),
+            )));
+  }
+  
+  TableRow _buildSupportOption(String title, {bool checkboxState, Function onChanged(bool newState), Widget icon, String doneText}) {
+    return TableRow(children: [
+      TableCell(
+        child: CheckboxListTile(
+          activeColor: ICON_INACTIVE,
+          secondary: icon,
+//              subtitle: Text(doneText ?? '', style: TextStyle(fontStyle: FontStyle.italic),),
+          title: Text(
+            title,
+            style: TextStyle(
+              decoration: checkboxState ? TextDecoration.lineThrough : TextDecoration.none,
+              color: checkboxState ? TEXT_INACTIVE : TEXT_ACTIVE,
+            ),
+          ),
+          dense: true,
+          value: checkboxState,
+          onChanged: onChanged,
+        ),
+      ),
+      // done text
+      TableCell(
+        verticalAlignment: TableCellVerticalAlignment.middle,
+        child: Text(
+          doneText ?? '',
+          style: TextStyle(
+            color: checkboxState ? TEXT_INACTIVE : TEXT_ACTIVE,
+          ),
+        ),
+      ),
+    ]);
   }
 
   /// Pushes [newScreen] to the top of the navigation stack using a fade in
@@ -565,6 +569,16 @@ class _PatientScreenBodyState extends State<_PatientScreenBody> {
       // viral load history
       setState(() {});
     });
+  }
+
+  void _pushPreferenceAssessmentScreen(BuildContext context, Patient patient) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return PreferenceAssessmentScreen(patient);
+        },
+      ),
+    );
   }
 
   void _pushARTRefillScreen(BuildContext context, Patient patient, String nextRefillDate) {
