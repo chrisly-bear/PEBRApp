bd9efbd67e2fd8e6e597b7a4201200d3d45bdede
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-06-19 16:06:04 +0200

    Created method stub for art refill date upload.

--- lib/screens/ARTRefillScreen.dart
+++ lib/screens/ARTRefillScreen.dart
@@ -6,6 +6,7 @@ import 'package:pebrapp/database/models/ARTRefill.dart';
 import 'package:pebrapp/database/models/Patient.dart';
 import 'package:pebrapp/screens/ARTRefillNotDoneScreen.dart';
 import 'package:pebrapp/state/PatientBloc.dart';
+import 'package:pebrapp/utils/Utils.dart';
 
 class ARTRefillScreen extends StatelessWidget {
   final Patient _patient;
@@ -42,7 +43,7 @@ class ARTRefillScreen extends StatelessWidget {
     if (newDate != null) {
       final ARTRefill artRefill = ARTRefill(this._patient.artNumber, RefillType.CHANGE_DATE(), nextRefillDate: newDate);
       await PatientBloc.instance.sinkARTRefillData(artRefill);
-      // TODO: upload the new date to the viral load database and if it didn't work show a message that the upload has to be retried manually
+      _uploadARTRefillDate(context, newDate);
       Navigator.of(context).popUntil((Route<dynamic> route) {
         return route.settings.name == '/patient';
       });
@@ -54,11 +55,22 @@ class ARTRefillScreen extends StatelessWidget {
     if (newDate != null) {
       final ARTRefill artRefill = ARTRefill(this._patient.artNumber, RefillType.DONE(), nextRefillDate: newDate);
       await PatientBloc.instance.sinkARTRefillData(artRefill);
-      // TODO: upload the new date to the viral load database and if it didn't work show a message that the upload has to be retried manually
+      _uploadARTRefillDate(context, newDate);
       Navigator.of(context).popUntil((Route<dynamic> route) {
         return route.settings.name == '/patient';
       });
     }
+  }
+
+  Future<void> _uploadARTRefillDate(BuildContext context, DateTime date) async {
+    // TODO: upload the new date to the viral load database and if it didn't work show a message that the upload has to be retried manually
+    await Future.delayed(Duration(seconds: 3));
+    showFlushBar(context, 'Please upload the notifications preferences manually.', title: 'Upload of ART Refill Date Failed', error: true, buttonText: 'Retry\nNow', onButtonPress: () {
+      Navigator.of(context).popUntil((Route<dynamic> route) {
+        return route.settings.name != '/flushbarRoute';
+      });
+      _uploadARTRefillDate(context, date);
+    });
   }
 
   Future<DateTime> _showDatePicker(BuildContext context) async {
