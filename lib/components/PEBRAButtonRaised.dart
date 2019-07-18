fbac88477b401fdf8aab1aafb443fc54b806b3a1
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-07-18 17:09:44 +0200

    Show a warning upon pressing logout / transfer device and let user cancel the action.

--- lib/components/PEBRAButtonRaised.dart
+++ lib/components/PEBRAButtonRaised.dart
@@ -6,11 +6,13 @@ class PEBRAButtonRaised extends StatelessWidget {
   final String _buttonText;
   final onPressed;
   final Widget widget;
+  final Color color;
 
   /// If `onPressed` is null then the button is painted gray to show that it's
   /// deactivated. If a `widget` is passed, the `_buttonText` is ignored and the
-  /// `widget` is displayed instead.
-  const PEBRAButtonRaised(this._buttonText, {this.onPressed, this.widget}) : super();
+  /// `widget` is displayed instead. Pass a [color] to override the default blue
+  /// background color of the button.
+  const PEBRAButtonRaised(this._buttonText, {this.onPressed, this.widget, this.color}) : super();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +24,7 @@ class PEBRAButtonRaised extends StatelessWidget {
       ),
       child: RaisedButton(
         onPressed: this.onPressed,
-        color: RAISED_BUTTON,
+        color: color ?? RAISED_BUTTON,
         child: widget != null ? widget : Text(
           this._buttonText.toUpperCase(),
           style: TextStyle(
