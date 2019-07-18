f70408ca602a9850c655476d9f77b898e4076e50
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-07-18 17:18:27 +0200

    Do not use a minWidth and maxWidth for the PEBRAButtons by default.

--- lib/components/PEBRAButtonRaised.dart
+++ lib/components/PEBRAButtonRaised.dart
@@ -7,19 +7,21 @@ class PEBRAButtonRaised extends StatelessWidget {
   final onPressed;
   final Widget widget;
   final Color color;
+  final double minWidth;
+  final double maxWidth;
 
   /// If `onPressed` is null then the button is painted gray to show that it's
   /// deactivated. If a `widget` is passed, the `_buttonText` is ignored and the
   /// `widget` is displayed instead. Pass a [color] to override the default blue
   /// background color of the button.
-  const PEBRAButtonRaised(this._buttonText, {this.onPressed, this.widget, this.color}) : super();
+  const PEBRAButtonRaised(this._buttonText, {this.onPressed, this.widget, this.color, this.minWidth, this.maxWidth}) : super();
 
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(
-        maxWidth: 300,
-        minWidth: 150,
+        maxWidth: maxWidth ?? double.infinity,
+        minWidth: minWidth ?? 0.0,
         minHeight: 40,
       ),
       child: RaisedButton(
