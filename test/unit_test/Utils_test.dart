f8ab0d3f526a61ec709e201cac9e1bd37fcefd07
Christoph Schwizer <c.schwizer.dev@gmail.com>
2019-08-07 01:45:42 +0200

    Added a few date utils tests.

--- test/unit_test/Utils_test.dart
+++ test/unit_test/Utils_test.dart
@@ -4,7 +4,30 @@ import 'package:pebrapp/utils/Utils.dart';
 void main() {
   group('Date Formatting', () {
     // TODO: write date formatting unit tests
+    test('today', () {
+      final result = formatDateAndTimeTodayYesterday(DateTime.now());
+      expect(result.substring(0, 7), 'today, ');
+    });
+    test('yesterday', () {
+      final result = formatDateAndTimeTodayYesterday(DateTime.now().subtract(Duration(days: 1)));
+      expect(result.substring(0, 11), 'yesterday, ');
+    });
   });
+
+  group('Date Calculation', () {
+    test('today', () {
+      final result = differenceInDays(DateTime(2000, 12, 31, 23, 55), DateTime(2000, 12, 31, 0, 5));
+      expect(result, 0);
+    });
+    test('yesterday', () {
+      final result = differenceInDays(DateTime(2000, 12, 31, 23, 55), DateTime(2000, 12, 30, 0, 5));
+      expect(result, -1);
+    });
+    test('tomorrow', () {
+      final result = differenceInDays(DateTime(2000, 12, 31, 23, 55), DateTime(2001, 1, 1, 0, 5));
+      expect(result, 1);
+    });
+  });  
 
   group('SMS', () {
 
