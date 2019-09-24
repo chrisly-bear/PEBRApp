c7883de7a48636c8188f5f60d3eb50f3cda39ec0
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2019-09-24 10:43:49 +0200

    Add the PEBRA inactive greyed out button

---
+++ lib/components/PEBRAButtonInactive.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:pebrapp/utils/AppColors.dart';
+
+class PEBRAButtonInactive extends StatelessWidget {
+
+  final String _buttonText;
+  final onPressed;
+  final Widget widget;
+  final Color color;
+  final double minWidth;
+  final double maxWidth;
+
+  /// If `onPressed` is null then the button is painted gray to show that it's
+  /// deactivated. If a `widget` is passed, the `_buttonText` is ignored and the
+  /// `widget` is displayed instead. Pass a [color] to override the default blue
+  /// background color of the button.
+  const PEBRAButtonInactive(this._buttonText, {this.onPressed, this.widget, this.color, this.minWidth, this.maxWidth}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: maxWidth ?? double.infinity,
+        minWidth: minWidth ?? 0.0,
+        minHeight: 40,
+      ),
+      child: RaisedButton(
+        onPressed: this.onPressed,
+        color: color ?? BUTTON_INACTIVE,
+        child: widget != null ? widget : Text(
+          this._buttonText.toUpperCase(),
+          style: TextStyle(
+            color: RAISED_BUTTON_TEXT,
+          ),
+        ),
+      ),
+    );
+  }
+}
